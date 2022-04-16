ec6c6ff9c02d489b0ffda93aca65891ef1a50871
techvailable@gmail.com <techvailable@gmail.com>
2022-04-16 14:43:37 +0500

    1

--- RentalWebService/DTOs/CategoryDto.cs
+++ RentalWebService/DTOs/CategoryDto.cs
@@ -5,7 +5,7 @@ namespace RentalWebService.DTOs
     public class CategoryDto : Dto
     {
         public Int64 Id { get; set; }
-        public Int64 StoreId { get; set; }
+        public Int64? StoreId { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public Store? Store { get; set; }
